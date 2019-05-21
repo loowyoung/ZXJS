4de26813a73e09b3b5bebc6315a28d4d829344a0
刘永 <liuy@safeinfo.com.cn>
2019-05-21 15:03:01 +0800

    修改oupt4文件读取路径；添加天地图秘钥

--- jisuan/src/main/java/com/anxin/jisuan/Controller/JisuanController.java
+++ jisuan/src/main/java/com/anxin/jisuan/Controller/JisuanController.java
@@ -4,6 +4,7 @@ import com.anxin.jisuan.model.CountVo;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,18 +23,21 @@ public class JisuanController {
     @Autowired
     private FileOperation fileOperation;
 
-    @ApiOperation(value="计算示例value", notes="计算示例notes")
+    @Value("${SLAB.filePath}")
+    private String SlabPath; //OUTPR4 文件位置
+
+    @ApiOperation(value = "计算示例value", notes = "计算示例notes")
     @GetMapping("/getCount")
     @ResponseBody
     public List<CountVo> getCount(double xc, double bx, double betax) {
         String[][] arr = new String[0][];
         try {
-            arr = fileOperation.getFileArray("F:/Projects/OUTPR4");
+            arr = fileOperation.getFileArray(SlabPath + "/OUTPR4");
         } catch (Exception e) {
             e.printStackTrace();
         }
         Count count = new Count();
-        List<CountVo> result = count.sum(arr,xc,bx,betax);
+        List<CountVo> result = count.sum(arr, xc, bx, betax);
         return result;
     }
 
